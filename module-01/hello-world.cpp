fbce49879163eaa86313f178d139b6940ab1d85d
Sepe <sepe.ahtosalo@gmail.com>
2026-01-15 12:38:15 +0200

    Add Module 1: Basic hello world program with comments

---
+++ module-01/hello-world.cpp
@@ -0,0 +1,9 @@
+// Testing my system works before course work: Basic Output
+// First C++ program - Hello World
+
+#include <iostream>
+
+int main() {
+    std::cout << "morooooo maaaaailmaaa!" << std::endl;
+    return 0;
+}
