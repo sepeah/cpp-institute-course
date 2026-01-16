44a4e104c865a3c3279ce5cb7c5e75f732fd3224
Sepe <sepe.ahtosalo@gmail.com>
2026-01-16 13:27:32 +0200

    Add notes.cpp with variable example and comments

---
+++ module-01/notes.cpp
@@ -0,0 +1,15 @@
+// A file for testing C++ features as they come along in the material
+
+/* 
+block comment 
+*/
+
+#include <iostream>
+
+int main() {
+    int Number = 66;
+    Number = Number + 1;
+    std::cout << "Number is: " << Number << std::endl;
+    return 0;
+
+}
