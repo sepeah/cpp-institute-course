6d607ccf3b42c18537c243ba75cbee7e197ca234
Sepe <sepe.ahtosalo@gmail.com>
2026-01-22 21:37:28 +0200

    Added notes on loops

--- module-02/notes_2.cpp
+++ module-02/notes_2.cpp
@@ -16,12 +16,26 @@ void function1();
 // types
 int function2();
 
+// while loops
+int function3();
+
+// do loops
+void function4();
+
+// for loops
+int function5();
+
+// break and continue
+int function6();
 
 int main(void)
 {
     function1();
     std::cout << "function2 returns: " << function2() << std::endl;
-
+    std::cout << "function3 looping...\n" << function3() << " LOOPS!" << std::endl;
+    function4();
+    function5();
+    function6();
     return 0;
 }
 
@@ -48,5 +62,49 @@ int function2() {
     long double pieceofpi = 3.1415926535897932384626433;
     bool truthvalue = true;
 
-    return pieceofpi;
+    return pieceofpi; // returns 3
 }
+
+int function3() {
+
+    int howmuch = 4, counter = 0;
+
+    while(counter < howmuch) {
+        std::cout << "LOOP" << std::endl;
+        counter += 1;
+    }
+    return counter;
+
+}
+
+void function4() {
+    int number = 3;
+    do {
+        function1();
+        number--;
+    }
+    while(number > 0);
+
+}
+
+int function5() {
+    for(int i = 0; i < 5; i++) {
+        std::cout << "For Loop iteration: " << i << std::endl;
+    }
+    return 0;
+}
+
+int function6() {
+    for(int i = 0; i < 10; i++) {
+        if(i == 3) {
+            std::cout << "Skipping 3" << std::endl;
+            continue;
+        }
+        if(i == 7) {
+            std::cout << "Breaking at 7" << std::endl;
+            break;
+        }
+        std::cout << "i is: " << i << std::endl;
+    }
+    return 0;
+}   
