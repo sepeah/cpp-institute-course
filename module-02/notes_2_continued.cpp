35511fb984d27d9c8eb79495e33b242481434751
Sepe <sepe.ahtosalo@gmail.com>
2026-01-27 11:17:30 +0200

    adds a function using 2d-matrix to notes

--- module-02/notes_2_continued.cpp
+++ module-02/notes_2_continued.cpp
@@ -9,20 +9,24 @@ The result will likely be a nonsensical mix of code snippets
 
 #include <iostream>
 
-int i;
+
 
 // switch statement example
 void function1();
 
+// array, matrix
+int function2();
+
 int main() {
 
     function1();
-    
+    std::cout << "function2 returns: " << function2() << std::endl;
     return 0;
 }
 
 void function1() {
 
+    int i;
     std::cout << "Enter a number between 1 and 4: ";
     std::cin >> i;
 
@@ -33,3 +37,24 @@ void function1() {
     case 4: std::cout << "OK" << std::endl; break;
     default: std::cout << "Don't care"  << std::endl;
     }
+}
+
+int function2() {
+    
+    int numbers[5] = {10, 20, 30, 40, 50};
+    char surname[4] = {'F', 'u', 'r', 'y'};
+
+    // chessboard representation with 2D array, counts and returns number of rooks placed
+    int chessboard[8][8] = {}; // Initialize all elements to 0 with = {}, otherwise contains garbage
+    const int ROOK = 1;
+    chessboard[0][0] = ROOK;
+    chessboard[0][7] = ROOK;
+    chessboard[7][0] = ROOK;
+    chessboard[7][7] = ROOK;
+    int rooks_sum = 0;
+    for (int counter = 0;counter<8;counter++)
+        for (int counter2 = 0;counter2<8;counter2++)
+            if (chessboard[counter][counter2]==ROOK) rooks_sum++;
+
+    return rooks_sum;
+}
