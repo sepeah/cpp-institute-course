56ce9efb71563423660804972999a2770ad2fa6e
Sepe <sepe.ahtosalo@gmail.com>
2026-02-02 11:20:25 +0200

    Adds module 3 notes

--- module-03/notes_3.cpp
+++ module-03/notes_3.cpp
@@ -3,7 +3,7 @@
  * Author: Sepe Ahtosalo
  * Date: January 29, 2026
  * A file for testing C++ features as they come along in the material of 
- * C++ Essentials 1 – Edube Interactive. module 2, part 1/2
+ * C++ Essentials 1 – Edube Interactive. module 3
  * https://cppinstitute.org/cpp-essentials-1
 
 
@@ -12,16 +12,27 @@
 
 #include <iostream>
 
+using namespace std;
 
 // pointers
 void function1();
 
+//sizeof operator
+int funtion2();
+
+//more on pointers, arrays
+void function3();
+
+//using function parameters
+void function4(unsigned short x, char c);
 
 int main() {
     function1();
+    funtion2();
+    function3();
+    function4(5,'#');
     return 0;
 }
-
 
 void function1() {
 int number;
@@ -29,6 +40,44 @@ int *pointer;
 number = 42;
 pointer = &number; // pointer now holds the address of number variable
 
-std::cout << "Value of number: " << number << std::endl;
-std::cout << "Address of number: " << &number << std::endl;
+
+cout << "Address: " << pointer << endl;
+cout << "Value of number in address: " << *pointer << endl;
+
+int *nullpointer = nullptr; // null pointer initialization
 }
+
+
+int funtion2() {
+// Displays sizes of fundamental data types
+    cout << "Size of char: " << sizeof(char) << " bytes\n"; 
+    cout << "Size of bool: " << sizeof(bool) << " bytes\n"; 
+    cout << "Size of short: " << sizeof(short) << " bytes\n"; 
+    cout << "Size of int: " << sizeof(int) << " bytes\n"; 
+    cout << "Size of long: " << sizeof(long) << " bytes\n"; 
+    cout << "Size of long long: " << sizeof(long long) << " bytes\n"; 
+    cout << "Size of float: " << sizeof(float) << " bytes\n"; 
+    cout << "Size of double: " << sizeof(double) << " bytes\n"; 
+    cout << "Size of long double: " << sizeof(long double) << " bytes\n"; 
+    cout << "Size of void*: " << sizeof(void*) << " bytes\n"; 
+    cout << sizeof(int* ) << " bytes for pointers in general" << endl;
+    return 0; 
+}
+
+void function3() {
+    int array1[] = {1,2,3,4,5};
+    int *pointer1, *pointer2, *pointer3;
+    pointer1 = array1;
+    pointer2 = pointer1+ 1;
+    pointer3 = pointer1 + 2;
+    cout << "First element: " << *pointer1 << endl;
+    cout << "Second element: " << *pointer2 << endl;
+    cout << "Third element: " << *pointer3 << endl;
+}
+
+void function4(unsigned short x,char c){
+    // prints a row of x characters c
+    for (int i=0;i<x;i++)
+    cout << c;
+    cout << endl;
+}  
